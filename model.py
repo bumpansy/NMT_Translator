692101769ef8d4b3408f9c9c5cb936e5a57e16f2
bumpansy <suyogkasture75@gmail.com>
2024-08-01 10:08:32 +0530

    Transofromer block

--- model.py
+++ model.py
@@ -165,3 +165,30 @@ class ProjectionLayer(nn.Module):
     def forward(self, x):
         return torch.log_softmax(self.proj(x), dim = -1)
 
+class Transformer(nn.Module):
+    def __init__(self, encoder: Encoder, decoder: Decoder, src_embed: InputEmbeddings, target_embed: InputEmbeddings, src_pos: PositionalEncoding, target_pos: PositionalEncoding, projection_layer: ProjectionLayer) -> None:
+        super().__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+        self.src_embed = src_embed
+        self.target_embed = target_embed
+        self.src_pos = src_pos
+        self.target_pos = target_pos
+        self.projection_layer = projection_layer
+
+    def encode(self, src, src_mask):
+        src = self.src_embed(src)
+        src = self.src_pos(src)
+        return self.encoder(src, src_mask)
+        
+    
+    def decode(self, encoder_output, src_mask, target, target_mask):
+        target = self.target_embed(target)
+        target = self.target_pos(target)
+        return self.decoder(target, encoder_output, src_mask, target_mask)
+    
+    def project(self, x):
+        return self.projection_layer(x)
+    
+
+def build_transformer()        
